cad48ab41f5baa91455ba9c3416ea0c683e76294
fujino-papy <papy41537@gmail.com>
2023-06-24 00:07:46 +0900

    任意のメッセージ

--- src/resources/views/management.blade.php
+++ src/resources/views/management.blade.php
@@ -18,52 +18,52 @@
         </div>
         <form class="form" action="/contacts" method="get">
             @csrf
-        <div class="search-table">
-            <table class="search-table_inner">
-                <tr class="search-table_content">
-                    <th class="search-table-category">お名前</th>
-                        <td class="search-table_name">
-                        <input class="name" type="text" name="family_name" value="{{ old('family_name') }}" />
-                        <input class="name" type="text" name="given_name" value="{{ old('given_name') }}" />
-                        </td>
-                    <th class="search-table-category_gender">性別</th>
-                    <td class="search-table_gender">
-                        <label class="radio">
+            <div class="search-table">
+                <table class="search-table_inner">
+                    <tr class="search-table_content">
+                        <th class="search-table-category">お名前</th>
+                            <td class="search-table_name">
+                            <input class="name" type="text" name="family_name" value="{{ old('family_name') }}" />
+                            <input class="name" type="text" name="given_name" value="{{ old('given_name') }}" />
+                            </td>
+                        <th class="search-table-category_gender">性別</th>
+                        <td class="search-table_gender">
+                            <label class="radio">
                             <input class="radio__btn" type="radio" name="gender" value="全て" checked="checked">
                                 <span class="radio__text">全て</span>
-                        </label>
-                        <label class="radio">
-                            <input class="radio__btn" type="radio" name="gender" value="男性">
-                                <span class="radio__text">男性</span>
-                        </label>
-                        <label class="radio">
-                            <input class="radio__btn" type="radio" name="gender" value="女性">
-                            <span class="radio__text">女性</span>
-                        </label>
-                    </td>
-                </tr>
-                <tr class="search-table_content">
-                    <th class="search-table-category">登録日</th>
-                    <td class="search-table_date">
+                            </label>
+                            <label class="radio">
+                                <input class="radio__btn" type="radio" name="gender" value="男性">
+                                    <span class="radio__text">男性</span>
+                            </label>
+                            <label class="radio">
+                                <input class="radio__btn" type="radio" name="gender" value="女性">
+                                <span class="radio__text">女性</span>
+                            </label>
+                        </td>
+                    </tr>
+                    <tr class="search-table_content">
+                        <th class="search-table-category">登録日</th>
+                        <td class="search-table_date">
                             <input class="date" type="date" name="from">
-                        <span class="mx-3">~</span>
+                            <span class="mx-3">~</span>
                             <input class="date" type="date" name="until">
-                    </td>
-                </tr>
-                <tr class="search-table_content">
-                    <th class="search-table-category">メールアドレス</th>
-                    <td class="search-table_email">
+                        </td>
+                    </tr>
+                    <tr class="search-table_content">
+                        <th class="search-table-category">メールアドレス</th>
+                        <td class="search-table_email">
                         <input class="email" type="email" name="email" value="{{ old('email') }}" />
-                    </td>
-                </tr>
-                <tr class="search-table_content">
-                    <td class="search-table_button">
+                        </td>
+                    </tr>
+                    <tr class="search-table_content">
+                        <td class="search-table_button">
                         <button class="search-button"  type="submit" name="search">検索</button>
                         <button class="reset-button" type="submit" name="reset">リセット</button>
-                    </td>
-                </tr>
-            </table>
-        </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
         </form>
     </div>
     {{$contacts->links('vendor.pagination.tailwind')}}
@@ -76,7 +76,7 @@
                 <th scope ="col">メールアドレス</th>
                 <th scope ="col">ご意見</th>
             </tr>
-            @foreach($contacts as $contacts);
+            @foreach($contacts as $contacts)
             <tr class="list-table_data">
                 <td>{{$contacts->id}}</td>
                 <td>{{$contacts->family_name}}</td>
